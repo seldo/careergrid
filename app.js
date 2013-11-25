3d6e57a8f344c5f11623b6a943d95ddc9c3d18ca
Laurie <github@seldo.com>
2013-11-25 00:31:08 -0800

    I spent a lot more time inventing middleware than I expected

--- app.js
+++ app.js
@@ -4,12 +4,17 @@
  */
 
 var express = require('express');
-var controllers = require('./controllers');
 var http = require('http');
 var path = require('path');
 //var passport = require('passport')
 //var LocalStrategy = require('passport-local')
 var mongoose = require('mongoose')
+var requireAll = require('require-all')
+var connect = require('connect')
+var expressSanitizer = require('./lib/express-sanitizer')
+
+// load controllers
+var controllers = requireAll(__dirname + '/controllers');
 
 var app = express();
 
@@ -19,12 +24,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(app.router);
 app.use(express.cookieParser())
-app.use(express.bodyParser())
-app.use(express.session({secret:'WTFBBQLOL'}))
+app.use(connect.bodyParser())
+app.use(expressSanitizer())
 //app.use(passport.initialize())
 //app.use(passport.session())
+app.use(express.session({secret:'WTFBBQLOL'}))
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -35,9 +41,11 @@ if ('development' == app.get('env')) {
 // connect to DB
 mongoose.connect('mongodb://localhost/careergrid');
 
-app.get('/', controllers.index);
-app.get('/build', routes.build);
+// define routes
+app.get('/', controllers.index.index);
+app.all('/build', controllers.index.build);
 
+// start the server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,23 @@
+var _ = require('underscore')
 
-/*
- * GET home page.
- */
-
+// homepage
 exports.index = function(req, res){
   res.render('index', {
-    title: 'Welcome to CareerGrid&trade;!'
-  });
+    skills: {
+      'Frontend': ['CSS','HTML','JavaScript'],
+      'Application': ['PHP','Python','Ruby','Java'],
+      'Frameworks': ['Rails','Sinatra','Express','Django','Flask','Zend'],
+      'Data store': ['SQL','MySQL','Postgres','Oracle','MongoDB','Redis'],
+      'Operations': ['Apache','Nginx','HAProxy','Memcache','RabbitMQ','Bash']
+    }
+  })
 };
 
+// grid builder
 exports.build = function(req, res){
-  // In reality we would sanitize the fuck out of this input
   res.render('build', {
-    start: req.param('start'),
-    end: req.param('end')
+    start: req.body.start,
+    end: req.body.end,
+    skills: req.body.skills
   })
 }
